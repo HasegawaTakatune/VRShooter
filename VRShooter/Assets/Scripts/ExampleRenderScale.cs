5392b6aa785fdd54c416180fe3d870f8d37af6f8
Takatune <0ta5274738t4p4r@gmail.com>
2019-11-18 21:34:03 +0900

    フェード・Aimeの作成 敵キャラのコアパーツを作成

---
+++ VRShooter/Assets/Scripts/ExampleRenderScale.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+namespace VRStandardAssets.Examples
+{
+    public class ExampleRenderScale : MonoBehaviour
+    {
+        [SerializeField] private float m_RenderScale = 1.5f;
+
+        private void Start()
+        {
+            XRSettings.eyeTextureResolutionScale = m_RenderScale;
+        }
+    }
+}
---
+++ VRShooter/Assets/Scripts/VRDeviceManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace VRStandardAssets.Utils
+{
+    /// <summary>
+    /// レンダリングパフォーマンスを調整するためのスクリプト
+    /// </summary>
+    public class VRDeviceManager : MonoBehaviour
+    {
+        [SerializeField] private float m_RenderScale = 1.4f;
+        private static VRDeviceManager s_instance;
+
+        public static VRDeviceManager Instance
+        {
+            get
+            {
+                if (s_instance == null)
+                {
+                    s_instance = FindObjectOfType<VRDeviceManager>();
+                    // 新しいシーンを読み込んでもオブジェクトが自動で破棄されないように登録
+                    DontDestroyOnLoad(s_instance.gameObject);
+                }
+                return s_instance;
+            }
+        }
+
+        private void Awake()
+        {
+            if (s_instance == null)
+            {
+                s_instance = this;
+                DontDestroyOnLoad(this);
+            }
+            else if (this != s_instance)
+                Destroy(gameObject);
+        }
+    }
+}
+
