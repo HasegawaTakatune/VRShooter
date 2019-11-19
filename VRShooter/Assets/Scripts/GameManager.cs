a48fdc37cca881329d37a67f95e54c4624bfd124
Takatune <0ta5274738t4p4r@gmail.com>
2019-11-19 22:17:06 +0900

    敵の外殻・コアの挙動を追加

---
+++ VRShooter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static float Score { get; set; }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameManager();
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+}
