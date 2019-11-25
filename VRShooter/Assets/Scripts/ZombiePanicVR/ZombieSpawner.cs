4cd26bff1fcea6f97d0ccdf72149891ac07ab40c
Takatune <0ta5274738t4p4r@gmail.com>
2019-11-25 20:29:46 +0900

    スポナー作成のみ

---
+++ VRShooter/Assets/Scripts/ZombiePanicVR/ZombieSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour
+{
+    const float Interval = 3.0f;
+
+    [SerializeField] private GameObject prefab;
+
+    public bool active = true;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        Vector3 Point = transform.position;
+        Quaternion Rota = transform.rotation;
+
+        while (active)
+        {
+            yield return new WaitForSeconds(1.0f);
+            Point.x = Random.Range(-Interval, Interval);
+            Instantiate(prefab, Point, Rota);
+        }
+    }
+}
